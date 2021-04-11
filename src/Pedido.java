deca6000a0b05f45f7ee8a5ba948cbdddae30506
Rafael Santos <40042652+rafaelsnv@users.noreply.github.com>
2021-04-10 22:17:05 -0300

    Adicionadas classes Produto e Pedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -2,5 +2,42 @@ public class Pedido {
    private String cliente;
    private int totalProdutos;
    private int prazo;
-   private Produto produto;
+
+   public Pedido (String qualCliente, int qualTotal, int qualPrazo){
+      this.cliente = qualCliente;
+      this.totalProdutos = qualTotal;
+      this.prazo  = qualPrazo;
+   }
+
+   public void setCliente(String cliente) {
+      this.cliente = cliente;
+   }
+
+   public String getCliente() {
+      return cliente;
+   }
+
+   public void setPrazo(int prazo) {
+      this.prazo = prazo;
+   }
+
+   public int getPrazo() {
+      return prazo;
+   }
+
+   public void setTotalProdutos(int totalProdutos) {
+      this.totalProdutos = totalProdutos;
+   }
+
+   public int getTotalProdutos() {
+      return totalProdutos;
+   }
+
+   @Override
+   public String toString() {
+      return "Pedido" +
+              "\nCliente: '" + cliente +
+              "\nNúmero total de produtos: " + totalProdutos +
+              "\nPrazo: " + prazo;
+   }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -2,6 +2,31 @@ public class Produto {
    private String tipo;
    private double volume;
 
+   public Produto (String qualTipo, double qualVolume){
+      this.tipo = qualTipo;
+      this.volume = qualVolume;
+   }
 
+   public void setVolume(double qualVolume) {
+      this.volume = qualVolume;
+   }
 
+   public double getVolume() {
+      return volume;
+   }
+
+   public void setTipo(String qualTipo) {
+      this.tipo = qualTipo;
+   }
+
+   public String getTipo() {
+      return tipo;
+   }
+
+   @Override
+   public String toString() {
+      return "Produto"+
+              "\ntipo: " + tipo +
+              "\nvolume: " + volume;
+   }
 }
