d2f7e8fab7fa094c992d283b260b809be26859f1
Rafael Santos <40042652+rafaelsnv@users.noreply.github.com>
2021-04-12 08:28:49 -0300

    Revert "Merge branch 'main' of https://github.com/rafaelsnv/SO_EmpacoMagic into main"
    This reverts commit 00126440fa2596600f3d1b48e2081ced47595fee, reversing
    changes made to 92800558a3bc1cd9c36637d4b28a30df3323ca19.

--- src/Leitor.java
+++ src/Leitor.java
@@ -4,25 +4,24 @@ import java.util.ArrayList;
 public class Leitor {
    private File dadosEmpacota;
 
-   public Heap lerArquivo(String nomeArquivo) throws IOException {                                             // O método lê cada linha do .txt, transforma em pedido e retorna uma Heap de pedidos
-      dadosEmpacota = new File(nomeArquivo);                                                                   // Colocar nos atributos o nome do arquivo de texto + '.txt'. Deve estar na pasta mãe.
+   public ArrayList lerArquivo(String nomeArquivo) throws IOException {                                        // Colocar nos atributos o nome do .txt. Deve estar na pasta mãe.
+      dadosEmpacota = new File(nomeArquivo);
       
-      Heap pedidos = new Heap();                                                                               // Heap vazia para a inserção dos pedidos
-      Pedido aux;                                                                                              // Auxiliar Pedido. Vai ser subscrito conforme cada linha for lida
-      String[] divisao;                                                                                        // Vetor de strings com cada categoria do .txt
+      ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
+      Pedido aux;
+      String[] divisao;
 
       BufferedReader br = new BufferedReader(new FileReader(dadosEmpacota));
 		
-		for (String linha; (linha = br.readLine()) != null;) { 							                              // Cada linha é lida, transformada em pedido e adicionada na Heap
+		for (String linha; (linha = br.readLine()) != null;) { 							                              // Cada linha é lida, transformada em pedido e adicionada na ArrayList
 			if (!linha.equals("")) {                                                                              // Se a linha estiver vazia, é ignorada
             divisao = linha.split(";");                                                                        
             if (divisao.length == 3){                                                                          // Se a linha não estiver no padrão de 3 categorias separadas por ";", ela é ignorada
                aux = new Pedido(divisao[0], Integer.parseInt(divisao[1]), Integer.parseInt(divisao[2]));       // Inicializa o auxiliar de Pedidos com os itens da linha
-               pedidos.add(aux);                                                                               // Adiciona o pedido na Heap
+               pedidos.add(aux);                                                                               // Adiciona o pedido no ArrayList
             }
 			}
 		}
       return pedidos;
    }
-
 }
