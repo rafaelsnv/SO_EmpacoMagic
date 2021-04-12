cb6afafc2cbbec9d5b75f20158160bb03c66c0b4
Frederico Henrique <Frederico.Henrique.1992@gmail.com>
2021-04-12 07:01:03 -0300

    Inclusão da variavel PACOTE e primeiros comentários

--- src/Esteira.java
+++ src/Esteira.java
@@ -2,21 +2,32 @@ import java.util.ArrayList;
 
 public class Esteira {
    private static int CAPACIDADE = 5000;
+   private static int PACOTE = 250; 
    private static double TEMPO_ROLAMENTO = 0.5;
-   private static double TEMPO_EMPACOTAMENTO = 5;
+   private static double TEMPO_EMPACOTAMENTO = 5.0;
    private static Relogio HORA_INICIO = new Relogio(28800);
    private static Relogio HORA_FINAL = new Relogio(61200);
    private ArrayList tempoMedio = new ArrayList();
-
+   
    public Esteira() {
 
    }
 
+   /**
+    * O método deve retornar o tempo medio gasto para executar o lote de produtos.
+    * @return Sempre retorna o tempo medio de empacotamento do lote.
+    */
    public double getTempoMedio() {
-
+      return 0.0;
    }
 
+   /**
+    * 
+    * @param inicio  Hora de inicio do empacotamento
+    * @param fim     Hora do fim do empacotamento
+    * @return        Retorna a quantidade de pacotes produzidas.
+    */
    public int getQntPacotesParcial(Relogio inicio, Relogio fim) {
-
+      int 0;
    }
 }
