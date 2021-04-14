c984b08bc917a7cb8a6f048c7686a35b4ad0e326
Frederico Henrique <Frederico.Henrique.1992@gmail.com>
2021-04-14 20:53:17 -0300

    Implementação do método Empacotar e quantidade total de pacote e teste no APP

--- src/Esteira.java
+++ src/Esteira.java
@@ -1,60 +1,65 @@
-import java.util.ArrayList;
-
-
 public class Esteira {
    private static int CAPACIDADE = 5000;
-   private static int PACOTE = 250; 
+   private static int PACOTE = 250;
    private static double TEMPO_ROLAMENTO = 0.5;
    private static double TEMPO_EMPACOTAMENTO = 5.0;
-   private static Horario HORA_INICIO = new Horario(28800);
-   private static Horario HORA_FINAL = new Horario(61200);
-   private ArrayList tempoMedio = new ArrayList();
-   
-   public Esteira() {
+   private int qntPacotes = 0;
+   private int volumeTotal = 0;
+   private int totalProdutos = 0;
 
+   public Esteira() {
    }
 
    /**
     * O método deve retornar o tempo medio gasto para executar o lote de produtos.
+    * 
     * @return Sempre retorna o tempo medio de empacotamento do lote.
     */
    public double getTempoMedio() {
       return 0.0;
    }
 
+   
    /**
-    * O método serve para retornar a quantidade de pacotes possíveis de serem produzidos no intervalo de tempo inserido.
-    * @param inicio  Hora de inicio do empacotamento.
-    * @param fim     Hora do fim do empacotamento.
-    * @return        Retorna a quantidade possível de pacotes produzidos no intervalo de tempo.
+    * O método serve para retornar a quantidade de pacotes possíveis de serem
+    * produzidos no intervalo de tempo inserido.
+
+    * @param pedidosPrazo
+    * @param pedidosQtd
+    * @return Retorna a quantidade total de pacotes
     */
-   public int getQntPacotesParcial(Horario inicio, Horario fim) {
-      return 0;
-   }
-
-   public void empacotarPedidos(){
-      Heap Heap = new Heap();
-      boolean esteiraLivre = false;
-      int cargaAtual = 0;
-      Pedido aux;
-
-      /**
-       * Loop para preenchimento da esteira com capacidade de até 5000cm³ 
-       */
-      do {
-         aux = Heap.retorno();            // TOTAL DE PRODUTOS
-
-         if (cargaAtual<CAPACIDADE) {
-            cargaAtual += aux.getTotalProdutos();               // AUTOSOMA DO TOTAL DE PRODUTOS
-         }else{
-            cargaAtual -= aux.getTotalProdutos();               // REMOVE A QNT DO ULTIMO PEDIDO, CASO A ESTEIRA NÃO SUPORTE MAIS
-            Heap.insere(aux);                // RETORNA O PEDIDO PARA A HEAP
-            esteiraLivre = true;             // FECHA A PRODUÇÃO DA ESTEIRA
+   public int getQntPacotesTotal(HeapPrazo pedidosPrazo, HeapQtd pedidosQtd) {
+      for (int i = pedidosPrazo.size(); i > 0; i--) { // Loop para teste do carregamento e
+         HeapQtd teste = pedidosPrazo.retorno(); // impressão dos pedidos com prioridade
+         for (int j = teste.size(); j > 0; j--) {
+            Pedido pedi = teste.retorno();
+            qntPacotes += ((pedi.getTotalProdutos()*PACOTE)/CAPACIDADE);
          }
+      }
+      return qntPacotes; // retorna a quantidade de pacotes Total
+   }
+   /**
+    * Método para empacotar o elemento com maior prioridade
+    * @param pedidosPrazo
+    * @param pedidosQtd
+    * @return
+    */
+   public double empacotarPedido(HeapPrazo pedidosPrazo, HeapQtd pedidosQtd) {
 
-      } while (esteiraLivre == false);       // FIM DO LOOP DE PREENCHIMENTO
+      for (int i = 1; i > 0; i--) { // Loop para teste do carregamento e
+         HeapQtd qntProdutos = pedidosPrazo.retorno(); // impressão dos pedidos com prioridade
+         for (int j = 1; j > 0; j--) {
+            Pedido pedi = qntProdutos.retorno();
+            totalProdutos = pedi.getTotalProdutos();
 
-      double qtdDePacote = cargaAtual/PACOTE;                        // QNT DE PACOTE QUE A ESTEIRA IRA PRODUZIR
-      double tempoEmpacotando = qtdDePacote * TEMPO_EMPACOTAMENTO;   // TEMPO GASTO PARA EMPACOTAR OS PRODUTOS (Em segundos)
+         }
+      }
+
+      volumeTotal = totalProdutos * PACOTE;
+      qntPacotes = volumeTotal / CAPACIDADE;
+      return (qntPacotes * TEMPO_EMPACOTAMENTO) + (qntPacotes * TEMPO_ROLAMENTO); // Retorna o tempo do empacotamento em
+                                                                                  // segundos (Apenas do mais
+                                                                                  // prioritário)
+
    }
 }
