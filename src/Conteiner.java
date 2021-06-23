5e8e39dc2047ad17d537d76ad63351335d08b6b0
jptdias10 <64169878+jptdias10@users.noreply.github.com>
2021-06-22 22:17:15 -0300

    idade conteiner

--- src/Conteiner.java
+++ src/Conteiner.java
@@ -1,41 +1,44 @@
 public class Conteiner {
-    private static final int MAX_VOLUME_CONTEINER = 1000000;        // valores em cm³
-    private static final int trocaConteiner = 30;                   // valores em segundos
+    private static final int MAX_VOLUME_CONTEINER = 1000000; // valores em cm³
+    private static final int trocaConteiner = 30; // valores em segundos
+    private int idade = 0;
 
     private int ID;
     public final ListaPedidos listaPedidosUnicos;
 
     /**
-     * Construtor Vazio
-     * ID = 0
-     * listaPedidosUnicos = vazia
+     * Construtor Vazio ID = 0 listaPedidosUnicos = vazia
      */
-    public Conteiner(){
+    public Conteiner() {
         this.ID = 0;
         this.listaPedidosUnicos = new ListaPedidos();
     }
-   
+
     /**
      * Construtor com parâmetros
-     * @param ID                    ID de identificação do conteiner. Ideia inicial do ID ser = código do produto
-     * @param listaPedidosUnicos    É a lista de produtos correspondente ao código do produto importado
+     * 
+     * @param ID                 ID de identificação do conteiner. Ideia inicial do
+     *                           ID ser = código do produto
+     * @param listaPedidosUnicos É a lista de produtos correspondente ao código do
+     *                           produto importado
      */
     public Conteiner(int ID, ListaPedidos listaPedidosUnicos) {
         this.ID = ID;
-        this.listaPedidosUnicos = listaPedidosUnicos;    
+        this.listaPedidosUnicos = listaPedidosUnicos;
     }
 
     /**
      * Método serve para verificar se a lista está vazia
-     * @return False != 0  
+     * 
+     * @return False != 0
      */
-    public boolean isEmpty(){
+    public boolean isEmpty() {
         if (this.listaPedidosUnicos.size() != 0) {
-            return false;   
+            return false;
         }
-            return true;
+        return true;
     }
-    
+
     public int getID() {
         return ID;
     }
@@ -56,5 +59,4 @@ public class Conteiner {
         return MAX_VOLUME_CONTEINER;
     }
 
-    
 }
