34ac6b1e409bbe80454a49d6d7d5e426ecb99a13
jptdias10 <64169878+jptdias10@users.noreply.github.com>
2021-06-22 22:25:35 -0300

    Esqueleto SyncListConteiner

---
+++ src/SyncListConteiner.java
@@ -0,0 +1,47 @@
+public class SyncListConteiner {
+
+    /**
+     * Adiciona um conteiner na lista
+     * 
+     * @return
+     */
+    public boolean addConteiner() {
+
+    }
+
+    /**
+     * Verifica se o conteiner em questão está zerado de produtos
+     */
+    public boolean isEmpty() {
+
+    }
+
+    /**
+     * Método para trocar o conteiner de menor idade com o requisitado
+     * 
+     * @param novo (Conteiner) conteiner a ser inserido na lista
+     * @return
+     */
+    public boolean swapConteiner(Conteiner novo) {
+
+    }
+
+    /**
+     * 
+     * @return Retorna o id do conteiner com a menor idade
+     */
+    public int younger() {
+
+    }
+
+    /**
+     * Método para repor os produtos do conteiner
+     * 
+     * @param tipo (int) recebe o tipo do conteiner a ser reposto
+     * @return
+     */
+    public boolean refresh(int tipo) {
+
+    }
+
+}
