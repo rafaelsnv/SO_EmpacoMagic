1505b3ed21245a23c8785c54c2ce89fd0a679a14
Daniel T Nunes <nnsdtr.github@gmail.com>
2021-06-23 18:35:14 -0300

    Atualizando esteira

--- src/Esteira.java
+++ src/Esteira.java
@@ -3,20 +3,31 @@ public class Esteira extends Thread {
    private static final double TEMPO_EMPACOTAMENTO = 5;  // segundos
 
    private final int meuID;
-   public final SyncFila fila;
-   public final SyncRelatorio relatorio;
+   private final SyncFila fila;
+   private final SyncRelatorio relatorio;
+   private SyncListContainer containeres;
    public final Horario meuRelogio;
 
-   public Esteira(int ID, SyncFila fila, SyncRelatorio relatorio) {
+   public Esteira(int ID, SyncFila fila, SyncRelatorio relatorio, SyncListContainer containeres) {
       this.meuID = ID;
       this.fila = fila;
       this.relatorio = relatorio;
+      this.containeres = containeres;
       this.meuRelogio = new Horario();
    }
 
-   public void empacotar() {
+   public void empacotar() throws CloneNotSupportedException {
       if(fila.size() > 0) {
          Pedido pedido = fila.pop();
+
+         int qtdProdutos = pedido.getQtdProdutos();
+         Container cont = containeres.getContainer(pedido.getID());
+
+         if (qtdProdutos > cont.getQtdAtualProdutos()) {
+            qtdProdutos -= cont.getQtdAtualProdutos();
+            cont.reabastecer();
+            this.meuRelogio.addSeconds(cont.getTempoTroca());
+         }
 
          double tempoEmpacotamento = (TEMPO_EMPACOTAMENTO + TEMPO_ROLAMENTO) * pedido.getQtdPacotes();
          this.meuRelogio.addSeconds(tempoEmpacotamento);
